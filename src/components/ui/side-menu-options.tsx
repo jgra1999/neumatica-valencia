3fa2817d55f84099e700504829dbca28ee9606c9
Jose Rojas <jgra11.2010@gmail.com>
2024-02-05 12:27:04 -0400

    Agregado Disclosure al compontente de opcion

--- src/components/ui/side-menu-options.tsx
+++ src/components/ui/side-menu-options.tsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import { Disclosure } from '@headlessui/react'
+import { ChevronUpIcon } from '@heroicons/react/20/solid'
 
 interface Props {
 	name: string
+	children?: React.ReactNode
 }
 
-export default function SideMenuOptions({ name }: Props) {
+export default function SideMenuOptions({ name, children }: Props) {
 	return (
 		<li>
-			<a href='#' className='hover:text-primary text-lg font-medium'>
-				{name}
-			</a>
+			<Disclosure>
+				{({ open }) => (
+					<>
+						<Disclosure.Button
+							className={`flex justify-between items-center w-full hover:text-primary text-lg font-medium ${
+								open && 'text-primary'
+							}`}
+						>
+							<span>{name}</span>
+							<ChevronUpIcon
+								className={`${
+									open ? 'rotate-180 transform text-primary' : ''
+								} h-5 w-5`}
+							/>
+						</Disclosure.Button>
+						{children}
+					</>
+				)}
+			</Disclosure>
 		</li>
 	)
 }
