b5bb3d6c679977ea95298577ce502ed5f42a1f53
Jose Rojas <jgra11.2010@gmail.com>
2024-02-27 09:05:50 -0400

    agregado shadcn y componente carrusel y button

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
-import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
 
-import netlify from "@astrojs/netlify";
+import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
-  output: "server",
-  adapter: netlify()
-});
+	integrations: [
+		react(),
+		tailwind({
+			applyBaseStyles: false
+		})
+	],
+	output: 'server',
+	adapter: netlify()
+})
