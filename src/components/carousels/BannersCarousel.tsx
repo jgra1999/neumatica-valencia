c5d428c88870e51075b7cc351f66b3cd527d5503
Jose Rojas <jgra11.2010@gmail.com>
2024-03-15 11:51:13 -0400

    creados carrusel de lineas y banners

---
+++ src/components/carousels/BannersCarousel.tsx
@@ -0,0 +1,54 @@
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious
+} from '@/components/ui/carousel'
+import { brands } from '@/lib/brands'
+import { useState } from 'react'
+
+export function BannersCarousel() {
+	const [cursor, setCursor] = useState('cursor-grab')
+	return (
+		<Carousel
+			opts={{ loop: true, active: true }}
+			onMouseDown={() => setCursor('cursor-grabbing')}
+			onMouseUp={() => setCursor('cursor-grab')}
+			className={`min-w-[100wv] ${cursor}`}
+		>
+			<CarouselContent className='flex items-center'>
+				<CarouselItem className='w-full basis-full'>
+					<img
+						src='/banners/servicios.webp'
+						alt='hero image'
+						className='w-full aspect-[1536/400]'
+					/>
+				</CarouselItem>
+				<CarouselItem className='w-full basis-full'>
+					<img
+						src='/banners/neumatica.webp'
+						alt='hero image'
+						className='w-full aspect-[1536/400]'
+					/>
+				</CarouselItem>
+				<CarouselItem className='w-full basis-full'>
+					<img
+						src='/banners/refrigeracion.webp'
+						alt='hero image'
+						className='w-full aspect-[1536/400]'
+					/>
+				</CarouselItem>
+				<CarouselItem className='w-full basis-full'>
+					<img
+						src='/banners/hidraulica.webp'
+						alt='hero image'
+						className='w-full aspect-[1536/400]'
+					/>
+				</CarouselItem>
+			</CarouselContent>
+			<CarouselPrevious />
+			<CarouselNext />
+		</Carousel>
+	)
+}
--- src/components/ui/lines-carrousel.tsx
+++ src/components/ui/lines-carrousel.tsx
@@ -1,14 +1,42 @@
-import { Swiper, SwiperSlide } from 'swiper/react'
-import CardLines from '../CardLines'
-import { Navigation } from 'swiper/modules'
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious
+} from '@/components/ui/carousel'
 
-// Import Swiper styles
-import 'swiper/css'
-import 'swiper/css/navigation'
+import CardLines from '../CardLines'
 
 export default () => {
 	return (
-		<div className='max-w-[400px] sm:max-w-[700px] md:max-w-[850px] lg:max-w-[1100px] xl:max-w-[1400px]'>
+		<Carousel opts={{ loop: true }}>
+			<CarouselContent className='flex items-center'>
+				<CarouselItem className='md:basis-1/2 lg:basis-1/3 xl:basis-1/4'>
+					<CardLines name='Neumática' img_url='/lines/icon-lineas.png' href='/' />
+				</CarouselItem>
+				<CarouselItem className='md:basis-1/2 lg:basis-1/3 xl:basis-1/4'>
+					<CardLines name='Hidráulica' img_url='/lines/icon-hidra.png' href='/' />
+				</CarouselItem>
+				<CarouselItem className='md:basis-1/2 lg:basis-1/3 xl:basis-1/4'>
+					<CardLines
+						name='Refrigeración Industrial'
+						img_url='/lines/icon-refrigeracion.png'
+						href='/'
+					/>
+				</CarouselItem>
+				<CarouselItem className='md:basis-1/2 lg:basis-1/3 xl:basis-1/4'>
+					<CardLines name='Servicios' img_url='/lines/icon-servicios.png' href='/' />
+				</CarouselItem>
+			</CarouselContent>
+			<CarouselPrevious />
+			<CarouselNext />
+		</Carousel>
+	)
+}
+
+/* 
+<div className='max-w-[400px] sm:max-w-[700px] md:max-w-[850px] lg:max-w-[1100px] xl:max-w-[1400px]'>
 			<Swiper
 				spaceBetween={15}
 				slidesPerView={1}
@@ -46,32 +74,5 @@ export default () => {
 				</SwiperSlide>
 			</Swiper>
 		</div>
-	)
-}
 
-{
-	/* <Swiper
-			spaceBetween={10}
-			slidesPerView={1}
-			onSlideChange={() => console.log('slide change')}
-			onSwiper={(swiper) => console.log(swiper)}
-			modules={[Pagination, Navigation]}
-		>
-			<SwiperSlide>
-				<CardLines name='Neumática' img_url='/product1.png' href='/' />
-			</SwiperSlide>
-			<SwiperSlide>
-				<CardLines
-					name='Refrigeración Industrial'
-					img_url='/product3.png'
-					href='/'
-				/>
-			</SwiperSlide>
-			<SwiperSlide>
-				<CardLines name='Hidráulica' img_url='/product2.png' href='/' />
-			</SwiperSlide>
-			<SwiperSlide>
-				<CardLines name='Servicios' img_url='/product4.png' href='/' />
-			</SwiperSlide>
-		</Swiper> */
-}
+*/
